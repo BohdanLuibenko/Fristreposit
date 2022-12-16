cb08543f4287d79738092bce877fc5dcbfb35a0f
Bohdan Luibenko <bohdanluben@gmail.com>
2022-12-16 15:37:00 +0200

    again

--- Helloworldserv/src/main/java/com/srccodes/example/customservlet.java
+++ Helloworldserv/src/main/java/com/srccodes/example/customservlet.java
@@ -2,7 +2,6 @@ package com.srccodes.example;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
