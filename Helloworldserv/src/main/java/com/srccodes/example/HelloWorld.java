0c4097809234753fabb13b35c1522efeac3cff54
Bohdan Luibenko <bohdanluben@gmail.com>
2022-12-16 15:01:17 +0200

    no comments

--- Helloworldserv/src/main/java/com/srccodes/example/HelloWorld.java
+++ Helloworldserv/src/main/java/com/srccodes/example/HelloWorld.java
@@ -9,29 +9,16 @@ import java.io.IOException;
 import java.io.PrintWriter;
 public class HelloWorld extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public HelloWorld() {
         super();
     }
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath()); 
 		response.setContentType("text/html");
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>Hello World!</h1>");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- Helloworldserv/src/main/java/com/srccodes/example/customservlet.java
+++ Helloworldserv/src/main/java/com/srccodes/example/customservlet.java
@@ -8,44 +8,24 @@ import java.io.IOException;
 import java.io.PrintWriter;
 public class customservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public customservlet() {
         super();
     }
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		response.setContentType("text/html");
         PrintWriter printWriter = response.getWriter();
         printWriter.println("<h1>GET request</h1>");
 	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 		PrintWriter printWriter = response.getWriter();
 		printWriter.println("<h1>POST request</h1>");
 	}
-
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter printWriter = response.getWriter();
 		printWriter.println("<h1>PUT request</h1>");
 	}
-
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		PrintWriter printWriter = response.getWriter();
 		printWriter.println("<h1>DELETE request</h1>");
